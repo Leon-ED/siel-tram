58450e1c34e8b05dc2d331e39e36042a662f5a39
Leon-ED <edmeeleon@outlook.fr>
2026-01-04 06:07:09 +0100

    Le temps est désormais correctement décompté

--- src/components/SingleTime.vue
+++ src/components/SingleTime.vue
@@ -1,20 +1,31 @@
 <template>
   <div class="departure h-w-100" :data-index="departureOrder">
-    <div class="vehicle-order">{{ departureOrder + 1 }}<sup>e</sup> tram</div>
+    <div class="vehicle-order">
+      {{ departureOrder + 1
+      }}<sup>
+        <template v-if="departureOrder === 0">er</template>
+        <template v-else>e</template>
+      </sup>
+      tram
+    </div>
     <div class="departure-time-before-arrival">
       <div class="time-info" v-if="showMinutes">
-        <span class="minutes">{{ getMinutesFromDate(departure.time) }}</span>
+        <AnimatedNumber :time="Math.round(secondsBeforeArrival / 60).toString()" class="minutes" />
         <span class="unit">min</span>
       </div>
       <span v-if="departure.isAtStop" class="at-platform-text">à quai</span>
     </div>
     <span class="bar" v-if="departureOrder === 0"></span>
+    <Chenillard size="50cqh" v-if="isTimeNotReliable" />
   </div>
 </template>
 <script setup lang="ts">
 import type { Departure } from '@/types'
-import { getMinutesFromDate, getSecondesFromDate } from '@/utils'
-import { computed, ref, toRefs } from 'vue'
+import { getSecondesFromDate } from '@/utils'
+import { useNow } from '@vueuse/core'
+import { computed, toRefs } from 'vue'
+import Chenillard from './Chenillard.vue'
+import AnimatedNumber from './AnimatedNumber.vue'
 
 interface Props {
   departure: Departure
@@ -25,13 +36,19 @@ const NEGATIVE_THRESHOLD_SECONDS = -30
 const props = defineProps<Props>()
 const { departure, departureOrder } = toRefs(props)
 
-const secondsBeforeArrival = ref<number>(getSecondesFromDate(departure.value.time))
+const now = useNow({ interval: 4_000 })
+
+const secondsBeforeArrival = computed(() => {
+  const _tick = now.value
+
+  return getSecondesFromDate(departure.value.time, true, _tick.getTime())
+})
 
 /**
  * Sert à savoir s'il faut afficher les trois points jaunes ...
  */
 const isTimeNotReliable = computed<boolean>(() => {
-  return secondsBeforeArrival.value < NEGATIVE_THRESHOLD_SECONDS
+  return secondsBeforeArrival.value < NEGATIVE_THRESHOLD_SECONDS && !departure.value.isAtStop
 })
 const showMinutes = computed<boolean>(() => {
   return isTimeNotReliable.value === false && !departure.value.isAtStop
@@ -50,7 +67,7 @@ const showMinutes = computed<boolean>(() => {
 .vehicle-order {
   position: absolute;
   left: 5cqh;
-  top: 1cqh;
+  top: 3cqh;
   color: white;
   font-size: 7.5cqh;
 }
@@ -60,6 +77,14 @@ const showMinutes = computed<boolean>(() => {
   display: flex;
   align-items: baseline;
   justify-content: center;
+}
+.time-info {
+  display: flex;
+  align-items: baseline;
+}
+sup {
+  vertical-align: 40%;
+  font-size: 0.65em;
 }
 .minutes {
   font-family: 'IDFMBold', 'ParisineBold', sans-serif;
