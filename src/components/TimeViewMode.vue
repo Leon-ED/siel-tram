77114450edfdca15647ebb22d8bf61eeff3ce218
Leon-ED <edmeeleon@outlook.fr>
2026-02-05 09:13:43 +0100

    On affiche les '...' si on a qu'un seul départ à afficher

--- src/components/TimeViewMode.vue
+++ src/components/TimeViewMode.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="departure-times h-w-100">
     <ol class="departure-times-list h-w-100">
-      <li v-for="(departure, index) in departures.slice(0,2)" :id="departure.id" :key="departure.id" class="departure-time-item h-w-100">
+      <li v-for="(departure, index) in departures.slice(0,1)" :id="departure.id" :key="departure.id" class="departure-time-item h-w-100">
         <SingleTime :departure="departure" :departure-order="index" />
+      </li>
+      <li v-if="departures.length === 1" class="departure-time-item h-w-100">
+        <SingleTime :departure="FAKE_DEPARTURE" :departure-order="1" />
       </li>
     </ol>
   </div>
@@ -14,6 +17,17 @@ import SingleTime from './SingleTime.vue'
 interface Props {
   departures: Departure[]
 }
+const FAKE_DEPARTURE: Departure = {
+  id: 'fake',
+  time: new Date(Date.now() - 20 * 60 * 1000).toISOString(),
+  isAtStop: false,
+  branchId: 'fake',
+  lineId: 'fake',
+  branchName: 'fake',
+  destination: 'fake',
+  platform: 'fake',
+}
+
 defineProps<Props>()
 </script>
 <style scoped lang="css">
